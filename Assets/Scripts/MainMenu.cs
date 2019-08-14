8a06f606ea8a8ab7eceb3dbf202c57085315f0c0
carlw0S <carlosbj1230@gmail.com>
2019-08-14 16:34:23 +0200

    Added time limit selector to the Main Menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject timeLimitGO;
+    private Text timeLimitText;
+
+    public float timeLimit = 120;
+    public float maxTimeLimit = 300;
+
+    
+
+    void Awake()
+    {
+        timeLimitText = timeLimitGO.GetComponent<Text>();
+        updateTimeLimit();
+    }
+
+
+
     public void GotoGameScene()
     {
         SceneManager.LoadScene("Game");
@@ -13,5 +30,31 @@ public class MainMenu : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void increaseTimeLimit()
+    {
+        if (timeLimit < maxTimeLimit)
+            timeLimit += 30;
+        else
+            timeLimit = 0;
+        updateTimeLimit();
+    }
+
+    public void decreaseTimeLimit()
+    {
+        if (timeLimit > 0)
+            timeLimit -= 30;
+        else
+            timeLimit = maxTimeLimit;
+        updateTimeLimit();
+    }
+
+    private void updateTimeLimit()
+    {
+        if (timeLimit == 0)
+            timeLimitText.text = "∞";
+        else
+            timeLimitText.text = string.Format("{0}:{1:00}", (int) timeLimit / 60, timeLimit % 60);
     }
 }
