e4ad8b34c443a91d974d8ed1aac69166de7061e2
carlw0S <carlosbj1230@gmail.com>
2021-02-10 17:41:31 +0100

    Fixed the FPS counter lmao

--- Assets/Scripts/FPSCounter.cs
+++ Assets/Scripts/FPSCounter.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 public class FPSCounter : MonoBehaviour
 {
     private Text FPSCounterText;
+    private float deltas = 0;
+    private int count = 0;
 
     void Awake()
     {
@@ -17,7 +19,15 @@ public class FPSCounter : MonoBehaviour
 
     private void ShowFPS()
     {
-        int fps = (int)(1f / Time.unscaledDeltaTime);
+        int fps = (int)(1f / (deltas / count));
         FPSCounterText.text = fps.ToString() + " FPS";
+        deltas = 0f;
+        count = 0;
+    }
+
+    void Update()
+    {
+        deltas += Time.unscaledDeltaTime;
+        count += 1;
     }
 }
